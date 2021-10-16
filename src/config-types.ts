35edd63e1f1dcce5c3e725c9e3fc81ffd2401fe5
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2021-10-16 03:28:43 +0100

    Fix env var wrong naming, fix database connection issue and convert port to number

--- src/config-types.ts
+++ src/config-types.ts
@@ -1,4 +1,10 @@
 export enum ConfigFields {
   SENTRY_DSN = 'SENTRY_DSN',
   NODE_ENV = 'NODE_ENV',
+  DB_HOST = 'DB_HOST',
+  DB_PORT = 'DB_PORT',
+  DB_USER = 'DB_USER',
+  DB_PASSWORD = 'DB_PASSWORD',
+  DB_NAME = 'DB_NAME',
+  RUN_MIGRATIONS = 'RUN_MIGRATIONS',
 }
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,10 +1,12 @@
+import * as dotEnv from 'dotenv';
+dotEnv.config();
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
   host: `${process.env.DB_HOST}`,
-  port: 5432,
-  username: `${process.env.DB_PORT}`,
+  port: Number(process.env.DB_PORT),
+  username: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
