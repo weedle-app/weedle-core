d2bb1db5fb356917ef948ac6843a07960a91a192
funmiayinde <funmiayinde11@gmail.com>
2021-10-17 00:53:17 +0100

    pulling update from main

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,7 +4,7 @@ const config: ConnectionOptions = {
   type: 'postgres',
   host: `${process.env.DB_HOST}`,
   port: 5432,
-  username: `${process.env.DB_PORT}`,
+  username: `${process.env.DB_USERNAME}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
