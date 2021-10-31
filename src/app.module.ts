0dedaf79e4a58bee564aa5045c776ee458821043
funmiayinde <funmiayinde11@gmail.com>
2021-11-01 00:00:45 +0100

    fixed commit conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
+import { LogLevel } from '@sentry/types';
 import { CoreModule } from './rest/api/core/core.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailHelperService } from './_core/services/notifications/mail-helper.service';
 import { MarketingModule } from './rest/api/marketing/marketing.module';
 import { appConfigService } from './_core/services/app-config.service';
 import configuration from '../config/configuration';
 import { AuthModule } from './rest/api/auth/auth.module';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+import { ConfigFields } from './config-types';
 
 @Module({
   imports: [
@@ -18,8 +21,6 @@ import { AuthModule } from './rest/api/auth/auth.module';
       load: [configuration],
     }),
     TypeOrmModule.forRoot(appConfigService.getTypeOrmConfig()),
-<<<<<<< HEAD
-=======
     SentryModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (cfg: ConfigService) => ({
@@ -32,7 +33,6 @@ import { AuthModule } from './rest/api/auth/auth.module';
       }),
       inject: [ConfigService],
     }),
->>>>>>> develop
     MarketingModule,
   ],
   controllers: [],
